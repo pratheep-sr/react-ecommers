d83e240dd7db4447d2ee34b869d4f2566db50536
pratheep-sr <pratheepjenish75@gmail.com>
2026-01-09 23:56:39 +0530

    the value update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Component/Home";
 import About from "./Component/About";
 import Ourproducts from "./Component/Ourproducts";
